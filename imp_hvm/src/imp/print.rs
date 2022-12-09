ab0dca4de30f90c787973159ec6e429f484736a4
santi <leonardo.ribeiro.santiago@gmail.com>
2022-12-09 17:29:48 -0300

    forgot some `;` and `else`'s

--- imp_hvm/src/imp/print.rs
+++ imp_hvm/src/imp/print.rs
@@ -21,18 +21,18 @@ impl std::fmt::Display for Imp {
         }//this is so ugly, maybe there is a less convoluted way of doing this.
         write!(f, "match {} {{\n{}\n}}", expr, cases)
       },
-      Imp::IfElse { condition, true_case, false_case } => write!(f, "if {} then {} else {}", condition, true_case, false_case),
+      Imp::IfElse { condition, true_case, false_case } => write!(f, "if {} {{\n{}\n}} else {{\n{}\n}}", condition, true_case, false_case),
       Imp::ForElse { initialize, condition, afterthought, body, else_case } => {
-        write!(f, "for ({};{};{}) {{\n{}\n}}\n{}", initialize, condition, afterthought, body, else_case)
+        write!(f, "for ({};{};{}) {{\n{}\n}} else {{\n{}\n}}", initialize, condition, afterthought, body, else_case)
       },
-      Imp::ForInElse { target, iterator, body, else_case } => write!(f, "for {} in {} {{\n{}\n}}\n{}", target, iterator, body, else_case),
-      Imp::WhileElse { condition, body, else_case } => write!(f, "while {} {{\n{}\n}}\n{}", condition, body, else_case),
+      Imp::ForInElse { target, iterator, body, else_case } => write!(f, "for {} in {} {{\n{}\n}} else {{\n{}\n}}", target, iterator, body, else_case),
+      Imp::WhileElse { condition, body, else_case } => write!(f, "while {} {{\n{}\n}} else {{\n{}\n}}", condition, body, else_case),
       Imp::Label { name, stmt } => write!(f, "{}: {}", name, stmt),
       Imp::Return { value } => write!(f, "return {};", value),
-      Imp::Goto { name } => write!(f, "goto {}", name),
+      Imp::Goto { name } => write!(f, "goto {};", name),
       Imp::Continue => write!(f, "continue;"),
       Imp::Break => write!(f, "break;"),
       Imp::Pass => write!(f, "pass;"),
-    }    
+    }
   }
 }
